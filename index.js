86bf50953c984d02e34100f189afbe3b3eca8093
Aadhavan Sharma <107205013+AadhavanSharma@users.noreply.github.com>
2025-07-17 02:55:09 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,63 @@
+// const express = require("express");
+// const app = express();
+// const mongoose = require("mongoose");
+// const dotenv = require("dotenv");
+// const userRoute = require("./routes/users");
+// const pinRoute = require("./routes/pins");
+
+// dotenv.config();
+
+// app.use(express.json());
+
+
+// // mongoose 
+// //  .connect(`mongodb+srv://aadhavansharma5700:HgXPGVKhwLR3R4OK@cluster0.c9cmqpt.mongodb.net/?retryWrites=true&w=majority` , {
+// //         useNewUrlParser: true,
+// //         useUnifiedTopology: true,
+// //         useCreateIndex: true,   })   
+// //  .then(() => console.log("MongoDB connected!"))
+// //  .catch(err => console.log(err));
+// mongoose 
+//  .connect(`mongodb+srv://aadhavansharma5700:h6E12ZiQQLgs9Nql@cluster0.tdhn87a.mongodb.net/project-travel?retryWrites=true&w=majority` , {
+//         useNewUrlParser: true,
+//         useUnifiedTopology: true,
+//         useCreateIndex: true,   })   
+//  .then(() => console.log("MongoDB connected!"))
+//  .catch(err => console.log(err,"hi"));
+
+// app.use("/api/users", userRoute);
+// app.use("/api/pins", pinRoute);
+
+
+// app.listen(8800, () => {
+//   console.log("Backend server is running!");
+// });
+
+const express = require("express");
+const app = express();
+
+
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+dotenv.config();
+
+const userRoute = require("./routes/users");
+const pinRoute = require("./routes/pins");
+
+
+app.use(express.json());
+
+mongoose 
+ .connect(process.env.MONGO_URL, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true,   })   
+ .then(() => console.log("MongoDB connected!"))
+ .catch(err => console.log(err));
+
+app.use("/api/users", userRoute);
+app.use("/api/pins", pinRoute);
+
+app.listen(8800, () => {
+  console.log("Backend server is running!");
+});
